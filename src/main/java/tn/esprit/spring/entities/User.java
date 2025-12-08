86844aa15ad901531bfe725b570cf2603433cb0e
Mechergui Dina <dina.mechergui@esprit.tn>
2025-12-08 18:52:59 +0100

    Chap4 – Last Name

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -1,53 +1,43 @@
 package tn.esprit.spring.entities;
 
-
 import java.io.Serializable;
 import java.util.Date;
-
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 
 @Entity
-@Table(name = "T_USER")
+@Table(name = "t_user")
 public class User implements Serializable {
-
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	private Long id;   
-	
-	private String firstName; 
-	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "first_name") // correspond à la colonne MySQL first_name
+	private String firstName;
+
+	@Column(name = "last_name")  // correspond à la colonne MySQL last_name
 	private String lastName;
-	
+
+	@Column(name = "date_naissance") // correspond à la colonne MySQL date_naissance
 	@Temporal(TemporalType.DATE)
 	private Date dateNaissance;
-	
+
+	@Column(name = "role")
 	@Enumerated(EnumType.STRING)
-	Role role; 
-	
-	public User() {	}
+	private Role role;
+
+	public User() { }
 
 	public User(String firstName, String lastName, Date dateNaissance, Role role) {
-		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.dateNaissance = dateNaissance;
 		this.role = role;
 	}
 
-
 	public User(Long id, String firstName, String lastName, Date dateNaissance, Role role) {
-		super();
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -55,39 +45,27 @@ public class User implements Serializable {
 		this.role = role;
 	}
 
+	// Getters et setters
+	public Long getId() { return id; }
+	public void setId(Long id) { this.id = id; }
+
+	public String getFirstName() { return firstName; }
+	public void setFirstName(String firstName) { this.firstName = firstName; }
+
+	public String getLastName() {
+		return lastName + "-v1";
+	}
+	public void setLastName(String lastName) { this.lastName = lastName; }
+
+	public Date getDateNaissance() { return dateNaissance; }
+	public void setDateNaissance(Date dateNaissance) { this.dateNaissance = dateNaissance; }
+
+	public Role getRole() { return role; }
+	public void setRole(Role role) { this.role = role; }
 
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", dateNaissance="
-				+ dateNaissance + ", role=" + role + "]";
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", dateNaissance=" + dateNaissance + ", role=" + role + "]";
 	}
-
-
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lName) {
-		this.lastName = lName;
-	}
-	public Date getDateNaissance() {
-		return dateNaissance;
-	}
-	public void setDateNaissance(Date dateNaissance) {
-		this.dateNaissance = dateNaissance;
-	}
-	public Role getRole() {
-		return role;
-	}
-	public void setRole(Role role) {
-		this.role = role;
-	}
-	
-
 }
